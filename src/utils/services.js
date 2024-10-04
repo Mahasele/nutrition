eb0efa3e4d0f7f008bd05daabea19db4e6dc36b1
Mahasele <139785998+Mahasele@users.noreply.github.com>
2024-10-04 16:20:40 +0200

    Update services.js to show the images

--- src/utils/services.js
+++ src/utils/services.js
@@ -2,17 +2,17 @@ export let services = [
     {
         title:"Medical Nutrition Therapy",
         desc: "Medical Nutrition Therapy(MNT) is an envidence based, individualised nutrition process meant to treat certain medical conditions, it is a way to manage chronic health conditions. It can help you reduce symptoms of certain conditions and prevent futher health complications by following a personalized diet and meal plan.",
-        image:'/src/assets/therapy.jpg'
+        image:'assets/therapy.jpg'
     },
     {
         title: "Chronic Disease Management",
         desc:"Chronic disease are conditions that are long-term and often without a cure. Common examples include cancer, heart disease, diabetes, obesity, asthma, HIV & AIDs, Alzleimers disease as well as bipolar depression. The root causes of chronic diseases are largely behavioural and preventable while some are unpreventable. Therefor, nutrition can both prevent and manage chronic disease.",
-        image:'/src/assets/chronic.jpg'
+        image:'assets/chronic.jpg'
     },
     {
         title:"Weight Loss Programs",
         desc:"MHAH is Weight loss program is a highly personalised vergimen that helps to lose weight naturally without much changes to your existing lifestyle or diet. We do this by understanding your body constitution very deeply. Since our program is majorly nutrition based, its naturally a sustainable approach which has proven to form healthy food habits that is unique to your body and its needs.",
-        image:'/src/assets/download.jpeg',
+        image:'assets/download.jpeg',
         questions:[
             "Who is this program for?",
             "You have Body Mass Index (BMI) more than 30 (male) or 28.6 (Female)",
@@ -25,7 +25,7 @@ export let services = [
         subServise: {
             subTitle:"Weight Gain Program",
             desc:"Mia's Nutrition Corner Lesotho clinic helps you add those pounds to the body.",
-            image:'/src/assets/weightgain.jpg',
+            image:'assets/weightgain.jpg',
             subTitleQuestions:[
                 " Who's this program for?",
                 "You have BMI which is less than 18kg/m",
@@ -40,7 +40,7 @@ export let services = [
     {
         title:"Weight Gain Program",
         desc:"Mia's Nutrition Corner Lesotho clinic helps you add those pounds to the body.",
-        image:'/src/assets/weightgain.jpg',
+        image:'assets/weightgain.jpg',
         Questions:[
             " Who's this program for?",
             "You have BMI which is less than 18kg/m",
@@ -52,52 +52,52 @@ export let services = [
     {
         title:"Peodriatic Nutrition",
         desc:"Peodriatic nutrition is the maintenance of a proper well-balanced diet consisting of the essential nutrient and the adequate calorine intake necessary to promote growth and sustain the physiologic requirements at the various stages of a child development. We focus on all nutritional aspects regarding children, from birth up to 18 years of age.",
-        image:'/src/assets/peads.jpg',
+        image:'assets/peads.jpg',
     },
     {
         title:"Nutrition During Pregnancy, Lactating",
         desc:"Pregnancy nutrition and pregnancy to the nutrient intake and dietary planning that is under before, during and after pregnancy. For this reasons, the nutrition of the mother is important from before conception as well as throughout pregnancy and breastfeeding. Therefore, we give toilored guidance on how to eat healthy during pregnancy throughout.",
-        image:'/src/assets/pregnency.jpg',
+        image:'assets/pregnency.jpg',
     },
     {
         title:"Nutrition During Lactating",
         desc:"Lactation is the physiologic completion of the reproductive cycle, The martenal body prepares during pregnancy for lactation, not only by developing the breast to produce milk but also by stoning additional nutrients and energy for milk production. Therefore, the transition to fully sustaining an infant by breastfeeding should not be complex or require major nutritional adjustments for a woman. We help you through it all, healthy mothers = healthy children.",
-        image:'/src/assets/lactation.jpg',
+        image:'assets/lactation.jpg',
     },
     {
         title:"Non Communicable Diseases Management",
         desc:"Non Communicable Diseases are a leading cause of deaths in the world. The term refers to a group of conditions that are not mainly caused by an acute infection result in long term health consequences and often create a need for long term treatment and care. These conditions include cancers, Cardiovascular diseases, diabetes and chronic lung illnesses. Mia's Heal At Home, help you with good nutrition in managing and preventing non communicable diseases.",
-        image:'/src/assets/noncommunicable.jpg',
+        image:'assets/noncommunicable.jpg',
     },
     {
         title:"Elderly (Nutrition)",
         desc:"Dementia - a general term for loss of memory, language problem solving and other thinking abilities that are severe enough to interfere with daily life. Alleimer's is the most common cause of dementia. Dementia, commonly affect elderly (old people). due to depression, isolation, death of a spouse, medical issues.",
-        image:'/src/assets/erderly.jpg',
+        image:'assets/erderly.jpg',
     },
     {
         title:"Personalized Diets",
         desc:"A personalised diet is the concept of improving patient's health based on changes to their diet and exercise regime, ensuring they get the necessary nutrition and level of exercise to speed up their recovery or improve or maintain their health level. Mia's Heal At Home take in to consideration the following; age, gender, height or weight, BMI, diet and environment before giving personal diet.",
-        image:'/src/assets/personalised.jpg',
+        image:'assets/personalised.jpg',
     },
     {
         title:"Sports Nutrition & Athletic Nutrition",
         desc:"Application of nutrition knowledge to a practical daily eating plan focused on providing the fuel for physical work and optimizing athletic perfomance in competitive events while also promoting overall health and well being. Mia's Heal At Home counsels athletes on optimal nutrition for exercise training (match nutrition to training phases and goals), competition, recovery for exercise, weight management, hydration, imunity, disordered eating, travel and supplementation.",
-        image:'/src/assets/sport.jpg',
+        image:'assets/sport.jpg',
     },
     {
         title:"Supplements (Dietary Supplements)",
         desc:"Dietary supplements are manufactured products intended to supplement a person's diet by taking a pill, copsule, table, powder or liquid. A supplement can provider nutrients either extracted from food sources, or that are synthetic (in order to increase the quality of their consuption). MHAH helps you with light supplementation, we prescribe not to provide pills. We help we Scientifically proven supplements.",
-        image:'/src/assets/suplements.jpg',
+        image:'assets/suplements.jpg',
     },
     {
         title:"Eating Disorders & Disordered eating",
         desc:"Eating disorders are mental disoders defined by abnormal eating behaivour that adversely affect an person's physical or mantal health. They include binge eating, anorexia nervosa, bulimia nervosa, pica, rumination. We help you with taloired guidance through it all.",
-        image:'/src/assets/eating.jpg',
+        image:'assets/eating.jpg',
     },
     {
         title:"Nutrition counselling / Diet counselling",
         desc:"A process by which a health professional with a special training in nutrition helps people make healthy food choices and form healthy eating habits. We at Mia's Heal At Home, We Carefuly evaluates your eating habits to develop a plan that helps you make positive, lasting changes and achieve good results results of different illnesses, with good food/Nutrition.",
-        image:'/src/assets/counselling.jpg',
+        image:'assets/counselling.jpg',
     }
 ]
 
